61e0f29ff1206655e546a51252ee9353d8e15041
waisaed <waisaed@umich.edu>
2019-09-06 16:40:27 -0400

    Add layers and assets

---
+++ map_layers/markers.js
@@ -0,0 +1,82 @@
+const markers = {
+  markers: [
+    {
+      lake: "fuxian",
+      header: "Restricting use of Lake Fuxian",
+      body:
+        "The Second Brigade of the Integrated Administrative Law Enforcement of Xianhu is responsible for enforcing fishing bans in Fuxian lake and ensuring the safety of tourists. Authorities have banned swimming, motor-boat usage, washing cars in the lake, and have limited fishing.",
+      lat: 24.562222,
+      long: 102.8425,
+      image: "beach.jpg",
+      category: "ultimate"
+    },
+    {
+      lake: "fuxian",
+      header: "Promoting eco-tourism",
+      body:
+        "<em>Happy World</em> is a planned theme-park that will attempt to leverage Chengjian's proximity to water and archeological sites to draw tourists to the city and provide an alternate source of income for the region.",
+      lat: 24.671638,
+      long: 102.907714,
+      image: "fuxian-happy-world.png",
+      category: "ultimate"
+    },
+    {
+      lake: "fuxian",
+      header: "Buffer-zone around Lake Fuxian",
+      body:
+        "Authorities have instituted a 100m buffer-zone around lake Fuxian, inside of which agriculture, houses, hotels, and other business are banned. Families living and farming within the zone are relocated to apartment communities and provided with a living stipend to compensate them for their lost livelihoods. ",
+      lat: 24.624338,
+      long: 102.960157,
+      image: "fuxian-relocation-settlement.png",
+      category: "ultimate"
+    },
+    {
+      lake: "fuxian",
+      header: "Wetland Park Construction",
+      body:
+        "Wetland parks have been created to buffer the lakes from agricultural runoff and to let the community enjoy curated natural settings along the lakes.",
+      lat: 24.631769,
+      long: 102.905644,
+      image: "fuxian-north-shore-wetland.png",
+      category: "input"
+    },
+    {
+      lake: "xingyun",
+      header: "Algae Removal",
+      body:
+        "A project scheduled to launch in June 2019 will remove an estimated 400,000 cubic meters of concentrated algae pulp per year from lake Xingyun",
+      lat: 24.38194,
+      long: 102.809242,
+      image: "xingyun-algal-bloom-seperation.png",
+      category: "proximate"
+    },
+    {
+      lake: "xingyun",
+      header: "Pollution Interception and Control Projects",
+      body:
+        "<p>Networks of interception ditches, canals, and oxidation ponds around agricultural areas enable the trapping and reuse of agricultural runoff. </p><iframe width='400' height='225' src='https://www.youtube.com/embed/bwWepTczTI4' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>",
+      lat: 24.32194444,
+      long: 102.7575,
+      category: "input"
+    },
+    {
+      lake: "xingyun",
+      header: "Water Diversion",
+      body:
+        "Water is pumped into Xingyun lake as part of an emergency measure to replenish water supply during the dry season. ",
+      lat: 24.3134481,
+      long: 102.7596582,
+      image: "xingyun-water-diversion.png",
+      category: "proximate"
+    },
+    {
+      lake: "qilu",
+      header: "Haixian Second Wastewater Treatment Plant",
+      body: "Body 2",
+      lat: 24.138931,
+      long: 102.720777,
+      image: "fuxian-wastewater-treatment.png",
+      category: "input"
+    }
+  ]
+};
